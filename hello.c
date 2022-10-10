9bdfda83c551726856d9a881370366e7aaf899c2
BeatrizMH176 <115451033+BeatrizMH176@users.noreply.github.com>
2022-10-10 04:33:51 -0500

    actualizacion remota

--- hello.c
+++ hello.c
@@ -4,12 +4,10 @@ int main(){
     printf("Ya no recuerdo la sintaxis de c")
     saludo("Betty")
     return 0;
-<<<<<<< HEAD
 }
 
 void saludo(char* nombre){
     printf("Hola %s",nombre);
 }
-=======
-}//Comentarios en c
->>>>>>> 7b7a575f22261a107980bf4b61d8c306e62954e0
+//Comentarios en c
+
