463fed867379c4776188c7f29f82d4cc7d6656d0
BEATRIZMENDOZA0104 <beatrizmendoza176@gmail.com>
2022-10-10 04:19:08 -0500

    Funcion en hello.c

--- hello.c
+++ hello.c
@@ -2,5 +2,10 @@
 int main(){
     printf("Hello World!");
     printf("Ya no recuerdo la sintaxis de c")
+    saludo("Betty")
     return 0;
+}
+
+void saludo(char* nombre){
+    printf("Hola %s",nombre);
 }
